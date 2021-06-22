babcca07c89cfa810bd6901b902b4483f3126f1d
Kausik <kausikporey@yahoo.com>
2021-06-22 16:34:20 +0530

    Add ServerURL

--- src/URL.js
+++ src/URL.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://localhost:3001';
+const SERVER_URL = 'https://asscup.herokuapp.com';
 const CLIENT_URL = 'http://localhost:3000';
 
 export {SERVER_URL,CLIENT_URL}
--- src/components/client/Header.js
+++ src/components/client/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
             <div className="animation">
-                <p style={{padding:0,margin:0}} id="animated-text">Anandapur Sishu Sangha (ASS CUP 2021)</p>
+                <p style={{padding:'10px',margin:0}} id="animated-text">Anandapur Sishu Sangha (ASS CUP 2021)</p>
             </div>
             <div>
                     <p style={{margin:0,backgroundColor:'#335A7B',color:'#fff',paddingLeft:'10px',paddingBottom:'5px'}}>Matches</p>
